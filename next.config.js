17170f6a78d782a1b648c4c702563498b7fef088
Thanh Pham <zlatan.pham@gmail.com>
2021-07-10 20:11:16 +0700

    perf: build with preact

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 require('dotenv').config();
 
-module.exports = { env: {} };
+module.exports = {
+  env: {},
+  webpack: (config) => {
+    if (process.env.NODE_ENV === 'production') {
+      config.resolve.alias['react'] = 'preact/compat';
+      config.resolve.alias['react-dom'] = 'preact/compat';
+      config.resolve.alias['react-ssr-prepass'] = 'preact-ssr-prepass';
+    }
+
+    return config;
+  },
+};
