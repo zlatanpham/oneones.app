97479ee72ad60a8f3de889698fc87a7f6df14554
Thanh Pham <zlatan.pham@gmail.com>
2021-07-27 23:21:28 +0700

    chore: update *.svg type

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,5 +2,13 @@
 declare module '*.png';
 declare module '*.jpg';
 declare module '*.jpeg';
-declare module '*.svg';
 declare module '*.gif';
+
+declare module '*.svg' {
+  import React = require('react');
+  export const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement>
+  >;
+  const src: string;
+  export default src;
+}
